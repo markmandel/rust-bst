bf8ca5eeb796f71b4527c985f39232ef8a92d0b7
Mark Mandel <mark.mandel@gmail.com>
2015-01-28 15:38:10 +1100

    Starting work on a toString implementation.

--- src/tree.rs
+++ src/tree.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 struct Tree<T> {
     value: T,
     left: Option<Box<Tree<T>>>,
@@ -5,16 +7,25 @@ struct Tree<T> {
     parent: Option<Box<Tree<T>>>,
 }
 
+impl<T> fmt::Display for Tree<T> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.write_str("Whoo!")
+    }
+}
 
 #[cfg(test)]
 mod test {
     use super::{Tree};
 
+    #[test]
     fn test_creation() {
         let t: Tree<i32> = Tree{value: 32, left: None, right: None, parent: None};
-        assert_eq!(32, t.value)
-
-
+        assert_eq!(32, t.value);
     }
 
+    #[test]
+    fn test_string() {
+        let t: Tree<i32> = Tree{value: 32, left: None, right: None, parent: None};
+        assert_eq!("[ 32 ]", format!("{}", t))
+    }
 }
