e4db6d46ed5cca45fe1ceabf9813ee2c74d6c643
Mark Mandel <mark.mandel@gmail.com>
2015-01-28 16:13:24 +1100

    Getting somewhere!

--- src/tree.rs
+++ src/tree.rs
@@ -1,7 +1,7 @@
 use std::fmt;
 
 struct Tree<T> {
-    value: T,
+    value: Option<T>,
     left: Option<Box<Tree<T>>>,
     right: Option<Box<Tree<T>>>,
     parent: Option<Box<Tree<T>>>,
@@ -9,9 +9,14 @@ struct Tree<T> {
 
 impl<T: fmt::Display> fmt::Display for Tree<T> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let mut str = format!("[ {}", self.value);
+        let mut str = String::from_str("[ ");
 
-        str = str + " ]";
+        match self.value {
+            None => str.push_str("<NONE>"),
+            Some(ref n) => str.push_str(format!("{}", n).as_slice()),
+            };
+
+        str.push_str(" ]");
 
         f.write_str(str.as_slice())
     }
@@ -22,17 +27,21 @@ mod test {
     use super::{Tree};
 
     fn single_value_fixture() -> Tree<i32> {
-        Tree{value: 32, left: None, right: None, parent: None}
+        Tree{value: Some(32), left: None, right: None, parent: None}
     }
 
     #[test]
     fn test_creation() {
         let t = single_value_fixture();
-        assert_eq!(32, t.value);
+        assert_eq!(Some(32), t.value);
     }
 
     #[test]
     fn test_string() {
+        //empty tree
+        let t: Tree<i32> = Tree{value: None, left: None, right: None, parent: None};
+        assert_eq!("[ <NONE> ]", format!("{}", t));
+
         //simple result
         let t = single_value_fixture();
 
