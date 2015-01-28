236da423b3a3f413cec7573bae8a4bcae7dfad86
Mark Mandel <mark.mandel@gmail.com>
2015-01-28 15:04:52 +1100

    A basic test is passing.

--- src/tree.rs
+++ src/tree.rs
@@ -4,3 +4,17 @@ struct Tree<T> {
     right: Option<Box<Tree<T>>>,
     parent: Option<Box<Tree<T>>>,
 }
+
+
+#[cfg(test)]
+mod test {
+    use super::{Tree};
+
+    fn test_creation() {
+        let t: Tree<i32> = Tree{value: 32, left: None, right: None, parent: None};
+        assert_eq!(32, t.value)
+
+
+    }
+
+}
